c0bc4f2eaaae2e4ae38e63b5069ab62d596de59d
Kang Wang <kang.wang@hakai.org>
2018-03-24 21:58:42 -0700

    Wrote and exported functions in notes.js to add, list, get, and remove notes

--- app.js
+++ app.js
@@ -10,18 +10,17 @@ const argv = yargs.argv;
 let command = process.argv[2];
 
 console.log('Command: ', command);
-console.log('\nProcess Argv: ', process.argv, '\n');
 console.log('\nYargs Argv: ', argv, '\n');
 
 // Adding a new note:
 if (command === 'add') {
   notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
-  console.log('\nListing all notes!\n');
+  notes.getAll();
 } else if (command === 'read') {
-  console.log('\nFetching note!\n');
+  notes.getNote(argv.title);
 } else if (command === 'remove') {
-  console.log('\nRemoving note!\n');
+  notes.removeNote(argv.title);
 } else {
   console.log('\nCommand not recognized!\n');
 }
--- notes.js
+++ notes.js
@@ -1,6 +1,24 @@
 console.log('Starting notes.js!\n');
 
-module.exports.addNote = () => {
-  console.log('addNote');
-  return 'New note!';
-}
+let addNote = (title, body) => {
+  console.log(`\nAdding note titled '${title}' with body content '${body}'!\n`);
+};
+
+let getAll = () => {
+  console.log('\nGetting all notes!\n');
+};
+
+let getNote = (title) => {
+  console.log(`\nGetting note titled '${title}'!\n`);
+};
+
+let removeNote = (title) => {
+  console.log(`\nRemoving note titled '${title}'!\n`);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
+};

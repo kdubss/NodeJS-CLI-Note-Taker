1c3237bbb1d9fc54618663c6224571e30cc8daab
Kang Wang <kang.wang@hakai.org>
2018-03-24 21:31:15 -0700

    Added CLI user inputs through the process global object within Node.

--- app.js
+++ app.js
@@ -4,3 +4,19 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes.js');
+
+let command = process.argv[2];
+console.log('Command: ', command);
+
+// Adding a new note:
+if (command === 'add') {
+  console.log('\nAdding new note!\n');
+} else if (command === 'list') {
+  console.log('\nListing all notes!\n');
+} else if (command === 'read') {
+  console.log('\nFetching note!\n');
+} else if (command === 'remove') {
+  console.log('\nRemoving note!\n');
+} else {
+  console.log('\nCommand not recognized!\n');
+}

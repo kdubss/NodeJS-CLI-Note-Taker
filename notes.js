4b42fed0f4fa15462c66b49bda771ec87039489c
Kang Wang <kang.wang@hakai.org>
2018-03-24 18:16:43 -0700

    Basic templating set up for a Notes app

---
+++ notes.js
@@ -0,0 +1,10 @@
+console.log('Starting notes.js!\n');
+
+module.exports.addNote = () => {
+  console.log('addNote');
+  return 'New note!';
+}
+
+module.exports.add = (a, b) => {
+  return (a + b);
+}

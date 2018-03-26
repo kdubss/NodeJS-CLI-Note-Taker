4244c7dacbe577606abb3268fb8e6f0ef93152e5
Kang Wang <kang.wang@hakai.org>
2018-03-26 16:52:12 -0700

    Completed 'addNote' command

--- app.js
+++ app.js
@@ -14,7 +14,15 @@ console.log('\nYargs Argv: ', argv, '\n');
 
 // Adding a new note:
 if (command === 'add') {
-  notes.addNote(argv.title, argv.body);
+  const note = notes.addNote(argv.title, argv.body);
+  if (note) {
+    console.log('Note created');
+    console.log('--');
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
+  } else {
+    console.log('Note title taken');
+  }
 } else if (command === 'list') {
   notes.getAll();
 } else if (command === 'read') {
--- notes.js
+++ notes.js
@@ -1,11 +1,43 @@
 console.log('Starting notes.js!\n');
 
-let addNote = (title, body) => {
-  console.log(`\nAdding note titled '${title}' with body content '${body}'!\n`);
+const fs = require('fs');
+
+const fetchNotes = () => {
+  // Retrieving currently stored JSON data:
+  // - Will crash if file does not exist!
+  try {
+    let notesString = fs.readFileSync('notes-data.json')
+    return JSON.parse(notesString);
+  } catch (err) {
+    return [];
+  }
 };
 
-let getAll = () => {
+const saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+const addNote = (title, body) => {
+  // console.log(`\nAdding note titled '${title}' with body content '${body}'!\n`);
+  let notes = fetchNotes();
+  let note = {
+    title,
+    body
+  };
+  // Checking for Duplicate Note Titles:
+  const duplicateNotes = notes.filter((note) => note.title === title);
+  if (duplicateNotes.length === 0) {
+    // Updating current stored JSON data w/ newly added data!
+    notes.push(note);
+    // If no duplicate note titles, then save to file!
+    saveNotes(notes)
+    return note
+  }
+};
+
+const getAll = () => {
   console.log('\nGetting all notes!\n');
+  // Fetch all notes and display them to client.
 };
 
 let getNote = (title) => {

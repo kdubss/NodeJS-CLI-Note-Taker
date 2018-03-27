ae4faf0c72e060d3b948f86244b15a666052329a
Kang Wang <kang.wang@hakai.org>
2018-03-26 17:50:48 -0700

    added function to read note according to title

--- app.js
+++ app.js
@@ -26,9 +26,16 @@ if (command === 'add') {
 } else if (command === 'list') {
   notes.getAll();
 } else if (command === 'read') {
-  notes.getNote(argv.title);
+  const note = notes.getNote(argv.title);
+  if (note[0]) {
+    console.log('Note read');
+    console.log('--');
+    console.log(`Title: ${note[0].title}`);
+    console.log(`Body: ${note[0].body}`);
+  } else {
+    console.log('Note not found!');
+  }
 } else if (command === 'remove') {
-  // noteRemoved will return True or False
   const noteRemoved = notes.removeNote(argv.title);
   let message = noteRemoved ? 'Note was removed' : 'Note not found';
   console.log(message);
